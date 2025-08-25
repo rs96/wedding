926c3ed3397369eca8ab56caea461e8710ffa680
Rob Shipley <shipleymrobert@gmail.com>
2025-08-25 22:56:21 +0100

    some updates

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const numImages = 4;
 const letters = ["a", "b", "c", "d"];
 
 let i = 2;
+let image;
 
 const main = () => {
-  console.log("teststests");
-  const image = document.querySelector("#img");
   image.src = `images/nb${i}.jpeg`;
   i++;
   if (i === 13) {
@@ -13,9 +12,13 @@ const main = () => {
   }
 };
 
-setInterval(() => {
-  main();
-}, 1000);
-// document.onload(() => {
-// });
-// }, 1000);
+const init = () => {
+  image = document.querySelector("#img");
+  setInterval(() => {
+    main();
+  }, 750);
+};
+
+window.addEventListener("load", () => {
+  init();
+});
